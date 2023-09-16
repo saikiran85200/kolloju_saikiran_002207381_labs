57b567ac61fce1551cd2c6644e594d4783b59258
saikiran852000 <kolloju.s@northeastern.edu>
2023-09-15 20:29:37 -0400

    Added Lab-1 project - Simple swing UI

---
+++ Lab-1/src/model/Product.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author kollo
+ */
+public class Product {
+    private String name;
+    private String descr;
+    private String avNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getAvNum() {
+        return avNum;
+    }
+
+    public void setAvNum(String avNum) {
+        this.avNum = avNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    
+}
